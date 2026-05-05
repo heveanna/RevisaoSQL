3a4279d8cc65a12f4b0d050f97aebffaa9d4fce7
heveanna <annahevellynn@gmail.com>
2026-05-05 16:36:42 -0300

    feat: resolução de questão até 23

--- ComandosSQL/prettyquery.sql
+++ ComandosSQL/prettyquery.sql
@@ -1,4 +1,4 @@
-USE dados;
+USE PetShop;
 
 -- ## 1. SELECT e filtros
 -- 1. Liste todos os clientes cadastrados.
@@ -91,12 +91,11 @@ SELECT	DataHoraAgendado,
 
 -- 15. Liste as vendas feitas em um determinado mês.
 
-/* @ANO = 2024;
-@MES = 02;
+DECLARE @MES INT = 02;
 
-SELECT DataHora 
-	FROM Venda 
-	WHERE */
+SELECT DataHora AS 'MÊS'
+	FROM Venda WITH(NOLOCK)
+	WHERE MONTH(DataHora) = @MES
 
 -- 16. Liste os pagamentos com valor maior que R$ 100,00.
 
@@ -129,3 +128,143 @@ SELECT	Nome as Produto,
 		FORMAT(Preco, 'C', 'Pt-Br')
 	FROM Produto
 	WHERE Preco BETWEEN 20 AND 100;
+
+-- ## 2. JOINs básicos
+-- 21. Liste o nome de cada animal junto com o nome do seu cliente/dono.
+
+SELECT	an.Nome AS 'Animal',
+		cl.Nome AS 'Cliente'
+	FROM Cliente as cl WITH(NOLOCK)
+		JOIN Animal as an
+			ON an.Id = cl.Id;
+
+-- 22. Liste o nome do animal, a espécie e a raça.
+
+SELECT	ra.Nome AS 'Raça',
+		an.Nome AS 'Animal',
+		es.Nome AS 'Espécie'
+	FROM Animal as an WITH(NOLOCK)
+		JOIN Especie as es
+			ON es.Id = an.Id
+		JOIN Raca as ra
+			ON ra.IdEspecie = es.Id;
+
+-- 23. Liste todos os animais com seus respectivos donos, mesmo que algum dado relacionado esteja incompleto.
+
+SELECT	an.Nome AS 'Animal', 
+
+-- 24. Liste todos os clientes e seus animais, incluindo clientes que ainda não têm animal cadastrado.
+
+-- 25. Liste os produtos junto com suas categorias.
+
+-- 26. Liste as vendas junto com o nome do cliente que realizou a compra.
+
+-- 27. Liste os pagamentos junto com a venda correspondente e o tipo de pagamento usado.
+
+-- 28. Liste os produtos vendidos em cada venda.
+
+-- 29. Liste o nome do produto, quantidade vendida e valor unitário em cada item de venda.
+
+-- 30. Liste os serviços vendidos em cada venda.
+
+-- 31. Liste o nome do serviço, nome do cliente e data da venda.
+
+-- 32. Liste os agendamentos com o nome do cliente, nome do animal e nome do funcionário responsável.
+
+-- 33. Liste os funcionários e suas especialidades.
+
+-- 34. Liste todos os funcionários, mesmo os que ainda não possuem especialidade cadastrada.
+
+-- 35. Liste todos os tipos de serviço e os funcionários especializados neles.
+
+-- 36. Liste os produtos e seus fornecedores.
+
+-- 37. Liste todos os fornecedores e os produtos que eles fornecem.
+
+-- 38. Liste os produtos que possuem mais de um fornecedor.
+
+-- 39. Liste os históricos de vacinação com o nome do animal e o nome do cliente.
+
+-- 40. Liste as alergias de cada animal com o nome do animal e do dono.
+
+-- ## 3. JOINs com filtros
+-- 41. Liste os animais da espécie `"Cachorro"`.
+
+-- 42. Liste os animais da raça `"Poodle"`.
+
+-- 43. Liste os clientes que possuem animais da espécie `"Gato"`.
+
+-- 44. Liste os produtos da categoria `"Medicamento"`.
+
+-- 45. Liste as vendas feitas por um cliente específico.
+
+-- 46. Liste todos os pagamentos feitos com cartão de crédito.
+
+-- 47. Liste os agendamentos de um funcionário específico.
+
+-- 48. Liste os agendamentos de uma data específica mostrando cliente, animal e serviço.
+
+-- 49. Liste as vacinas aplicadas em animais de um cliente específico.
+
+-- 50. Liste os produtos vendidos em vendas acima de R$ 200,00.
+
+-- 51. Liste os serviços vendidos para animais da espécie `"Cachorro"`.
+
+-- 52. Liste os fornecedores que fornecem produtos da categoria `"Ração"`.
+
+-- 53. Liste os funcionários especializados em `"Banho e Tosa"`.
+
+-- 54. Liste as vendas de produto feitas em determinado mês.
+
+-- 55. Liste os clientes que compraram determinado produto.
+
+-- 56. Liste os animais que possuem alergias cadastradas.
+
+-- 57. Liste os animais que não possuem alergias cadastradas.
+
+-- 58. Liste os produtos que nunca foram vendidos.
+
+-- 59. Liste os serviços que nunca foram vendidos.
+
+-- 60. Liste os clientes que nunca fizeram uma venda.
+
+-- ## 4. Agregações com JOIN
+-- 61. Mostre a quantidade de animais por cliente.
+
+-- 62. Mostre a quantidade de animais por espécie.
+
+-- 63. Mostre a quantidade de animais por raça.
+
+-- 64. Mostre a quantidade de produtos por categoria.
+
+-- 65. Mostre o total vendido por venda.
+
+-- 66. Mostre o total gasto por cada cliente.
+
+-- 67. Mostre a quantidade de vendas por cliente.
+
+-- 68. Mostre a quantidade de produtos vendidos por produto.
+
+-- 69. Mostre o faturamento total por produto.
+
+-- 70. Mostre o faturamento total por serviço.
+
+-- 71. Mostre a quantidade de agendamentos por funcionário.
+
+-- 72. Mostre a quantidade de agendamentos por status.
+
+-- 73. Mostre a quantidade de pagamentos por tipo de pagamento.
+
+-- 74. Mostre o valor total recebido por tipo de pagamento.
+
+-- 75. Mostre o total de vacinas aplicadas por animal.
+
+-- 76. Mostre o total de vacinas aplicadas por espécie.
+
+-- 77. Mostre os clientes com mais de 2 animais.
+
+-- 78. Mostre os produtos que venderam mais de 10 unidades.
+
+-- 79. Mostre os funcionários com mais de 5 agendamentos.
+
+-- 80. Mostre as categorias de produto com mais de 3 produtos cadastrados.
